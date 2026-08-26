7e5ad3f18f8e2ddb20e2ff31b1b9e7d2ab1b6a5d
Yash Patankar <yashrpatankar24@gmail.com>
2026-08-27 01:21:46 +0530

    Downloaded docker, redis for niche system design

--- Backend/CloudBox-Main/CloudBox/settings.py
+++ Backend/CloudBox-Main/CloudBox/settings.py
@@ -42,9 +42,22 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "corsheaders",
-    'accounts'
+    'accounts',
+    'redis'
 
 ]
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
