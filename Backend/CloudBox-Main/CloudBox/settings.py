bb58c79a0133dfd3530ca583f244bcdba6791d7c
Yash Patankar <yashrpatankar24@gmail.com>
2026-08-27 00:34:18 +0530

    Mail Issue solved

--- Backend/CloudBox-Main/CloudBox/settings.py
+++ Backend/CloudBox-Main/CloudBox/settings.py
@@ -146,9 +146,15 @@ MAILERS = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'yashrpatankar24@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_email_password'  
+MAILERS = {
+    "default": {
+        "BACKEND": "django.core.mail.backends.smtp.EmailBackend",
+        "OPTIONS": {
+            "host": "smtp.gmail.com",
+            "port": 587,
+            "username": "",
+            "password": "your_app_password", 
+            "use_tls": True,
+        },
+    },
+}
