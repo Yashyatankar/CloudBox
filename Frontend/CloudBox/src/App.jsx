01edfd038e6c8bbc61cd57c954721aee48c28928
Yash Patankar <yashrpatankar24@gmail.com>
2026-09-04 18:09:27 +0530

    Add react-router-dom and wrap App components in BrowserRouter

--- Frontend/CloudBox/src/App.jsx
+++ Frontend/CloudBox/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import AuthPage from './Components/AuthPage.jsx'
 import Panel from './Components/Panel.jsx'
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <AuthPage/>
-      <Panel />
+      <BrowserRouter>
+        <AuthPage />
+        <Panel />
+      </BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/dashboard" element={<Panel />} />
+      </Routes>
+
     </>
   )
 }
